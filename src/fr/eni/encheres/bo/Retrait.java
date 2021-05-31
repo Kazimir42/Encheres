df662b6f7ceae784f560b92e4d36878017247ac0
Rémi Champlon <champlonremii@gmail.com>
2021-05-31 17:19:44 +0200

    premier commit

---
+++ src/fr/eni/encheres/bo/Retrait.java
@@ -0,0 +1,45 @@
+package fr.eni.encheres.bo;
+
+public class Retrait extends ArticleVendu {
+	private String rue;
+	private int codePostal;
+	private String ville;
+	
+	
+	public Retrait() {
+		
+	}
+	
+	public Retrait(String rue, int codePostal, String ville) {
+		super();
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+	}
+	
+	public String getRue() {
+		return rue;
+	}
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	public int getCodePostal() {
+		return codePostal;
+	}
+	public void setCodePostal(int codePostal) {
+		this.codePostal = codePostal;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	
+	@Override
+	public String toString() {
+		return "Retrait [rue=" + rue + ", codePostal=" + codePostal + ", ville=" + ville + "]";
+	}
+	
+	
+}
